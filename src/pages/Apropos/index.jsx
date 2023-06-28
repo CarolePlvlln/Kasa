360fc4618092e9cadcca7e0cc07b93c7b9ec0584
Carole Pelvillain <pelvillain.carole@live.fr>
2023-06-28 15:03:22 +0200

    bouton a propos et icon

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -2,18 +2,50 @@
 import React from 'react'
 import backgroundApropos from '../../assets/backgroundApropos.png'
 import apropos from './apropos.scss'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
 function Apropos() {
     return (
       <div className='layout' style={apropos}>
         <img src={backgroundApropos} alt='img-background-a-propos' className='layout__img-background' />
+
         <div className='layout__infos'>
-          <button className='layout__infos--button'>Fiabilité</button>
-          <button className='layout__infos--button'>Respect</button>
-          <button className='layout__infos--button'>Service</button>
-          <button className='layout__infos--button'>Sécurité</button>
+
+          <div className='layout__infos--divBtn'>
+            <button className='bouton' type="button" data-toggle="collapse" href="#collapse1" onClick={()=> ""}>Fiabilité<FontAwesomeIcon icon={faChevronDown} className='icon'/></button>
+              <div class="collapse1 collapse">
+              <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+              régulièrement vérifiées par nos équipes.</p>
+              </div>
+            </div>
+
+            <div className='layout__infos--divBtn'>
+            <button className='bouton' type="button" data-toggle="collapse" href="#collapse2">Respect<FontAwesomeIcon icon={faChevronDown} className='icon'/></button>
+              <div class="collapse2 collapse">
+              <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+              perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+              </div>
+          </div>
+
+          <div className='layout__infos--divBtn'>
+          <button className='bouton'type="button" data-toggle="collapse" href="#collapse3">Service<FontAwesomeIcon icon={faChevronDown} className='icon'/></button>
+          <div class="collapse3 collapse">
+              <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+              perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+          </div>
+          </div> 
+
+          <div className='layout__infos--divBtn'>
+          <button className='bouton'type="button" data-toggle="collapse" href="#collapse4">Sécurité<FontAwesomeIcon icon={faChevronDown} className='icon'/></button>
+          <div class="collapse4 collapse">
+              <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+              correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+              locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+              également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+          </div>
         </div>
-      </div>
+        </div>
+     </div>
     )
   }
   
